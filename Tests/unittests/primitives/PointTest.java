bf4be81081d382a6763b4f9a8d8bd805ca66d6af
rafaelnajman <rafael.najman@hotmail.com>
2022-03-09 15:50:55 +0200

    start to do Junit tests

---
+++ Tests/unittests/primitives/PointTest.java
@@ -0,0 +1,18 @@
+package unittests.primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void subtract() {
+    }
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    void testAdd() {
+    }
+}
