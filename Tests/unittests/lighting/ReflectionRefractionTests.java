554d695a1db48cd78ccaa919c0b33964a435eb24
yehuda shani <yehudashani88@gmail.com>
2022-05-17 21:43:45 +0300

    started making picture

--- Tests/unittests/lighting/ReflectionRefractionTests.java
+++ Tests/unittests/lighting/ReflectionRefractionTests.java
@@ -21,90 +21,117 @@ import scene.Scene;
  * @author dzilb
  */
 public class ReflectionRefractionTests {
-	private Scene scene = new Scene("Test scene");
+    private Scene scene = new Scene("Test scene");
 
-	/**
-	 * Produce a picture of a sphere lighted by a spot light
-	 */
-	@Test
-	public void twoSpheres() {
-		Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-				.setVPSize(150, 150).setVPDistance(1000);
+    /**
+     * Produce a picture of a sphere lighted by a spot light
+     */
+    @Test
+    public void twoSpheres() {
+        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVPSize(150, 150).setVPDistance(1000);
 
-		scene.geometries.add( //
-				new Sphere(new Point(0, 0, -50), 50d).setEmission(new Color(BLUE)) //
-						.setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setKt(0.3)),
-				new Sphere(new Point(0, 0, -50), 25d).setEmission(new Color(RED)) //
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)));
-		scene.lights.add( //
-				new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
-						.setKl(0.0004).setKq(0.0000006));
+        scene.geometries.add( //
+                new Sphere(new Point(0, 0, -50), 50d).setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setKt(0.3)),
+                new Sphere(new Point(0, 0, -50), 25d).setEmission(new Color(RED)) //
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)));
+        scene.lights.add( //
+                new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2)) //
+                        .setKl(0.0004).setKq(0.0000006));
 
-		camera.setImageWriter(new ImageWriter("refractionTwoSpheres", 500, 500)) //
-				.setRayTracer(new RayTracerBasic(scene)) //
-				.renderImage() //
-				.writeToImage();
-	}
+        camera.setImageWriter(new ImageWriter("refractionTwoSpheres", 500, 500)) //
+                .setRayTracer(new RayTracerBasic(scene)) //
+                .renderImage() //
+                .writeToImage();
+    }
 
-	/**
-	 * Produce a picture of a sphere lighted by a spot light
-	 */
-	@Test
-	public void twoSpheresOnMirrors() {
-		Camera camera = new Camera(new Point(0, 0, 10000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-				.setVPSize(2500, 2500).setVPDistance(10000); //
+    /**
+     * Produce a picture of a sphere lighted by a spot light
+     */
+    @Test
+    public void twoSpheresOnMirrors() {
+        Camera camera = new Camera(new Point(0, 0, 10000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVPSize(2500, 2500).setVPDistance(10000); //
 
-		scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255), new Double3(0.1)));
+        scene.setAmbientLight(new AmbientLight(new Color(255, 255, 255), new Double3(0.1)));
 
-		scene.geometries.add( //
-				new Sphere(new Point(-950, -900, -1000), 400d).setEmission(new Color(0, 0, 100)) //
-						.setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.5)),
-				new Sphere(new Point(-950, -900, -1000), 200d).setEmission(new Color(100, 20, 20)) //
-						.setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20)),
-				new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500), new Point(670, 670, 3000)) //
-						.setEmission(new Color(20, 20, 20)) //
-						.setMaterial(new Material().setKr(1)),
-				new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500),
-						new Point(-1500, -1500, -2000)) //
-						.setEmission(new Color(20, 20, 20)) //
-						.setMaterial(new Material().setKr(0.5)));
+        scene.geometries.add( //
+                new Sphere(new Point(-950, -900, -1000), 400d).setEmission(new Color(0, 0, 100)) //
+                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20).setKt(0.5)),
+                new Sphere(new Point(-950, -900, -1000), 200d).setEmission(new Color(100, 20, 20)) //
+                        .setMaterial(new Material().setKd(0.25).setKs(0.25).setShininess(20)),
+                new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500), new Point(670, 670, 3000)) //
+                        .setEmission(new Color(20, 20, 20)) //
+                        .setMaterial(new Material().setKr(1)),
+                new Triangle(new Point(1500, -1500, -1500), new Point(-1500, 1500, -1500),
+                        new Point(-1500, -1500, -2000)) //
+                        .setEmission(new Color(20, 20, 20)) //
+                        .setMaterial(new Material().setKr(0.5)));
 
-		scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point(-750, -750, -150), new Vector(-1, -1, -4)) //
-				.setKl(0.00001).setKq(0.000005));
+        scene.lights.add(new SpotLight(new Color(1020, 400, 400), new Point(-750, -750, -150), new Vector(-1, -1, -4)) //
+                .setKl(0.00001).setKq(0.000005));
 
-		ImageWriter imageWriter = new ImageWriter("reflectionTwoSpheresMirrored", 500, 500);
-		camera.setImageWriter(imageWriter) //
-				.setRayTracer(new RayTracerBasic(scene)) //
-				.renderImage() //
-				.writeToImage();
-	}
+        ImageWriter imageWriter = new ImageWriter("reflectionTwoSpheresMirrored", 500, 500);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scene)) //
+                .renderImage() //
+                .writeToImage();
+    }
 
-	/**
-	 * Produce a picture of a two triangles lighted by a spot light with a partially
-	 * transparent Sphere producing partial shadow
-	 */
-	@Test
-	public void trianglesTransparentSphere() {
-		Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-				.setVPSize(200, 200).setVPDistance(1000);
+    /**
+     * Produce a picture of a two triangles lighted by a spot light with a partially
+     * transparent Sphere producing partial shadow
+     */
+    @Test
+    public void trianglesTransparentSphere() {
+        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVPSize(200, 200).setVPDistance(1000);
 
-		scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
+        scene.setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
 
-		scene.geometries.add( //
-				new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135), new Point(75, 75, -150)) //
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)), //
-				new Triangle(new Point(-150, -150, -115), new Point(-70, 70, -140), new Point(75, 75, -150)) //
-						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)), //
-				new Sphere(new Point(60, 50, -50), 30d).setEmission(new Color(BLUE)) //
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)));
+        scene.geometries.add( //
+                new Triangle(new Point(-150, -150, -115), new Point(150, -150, -135), new Point(75, 75, -150)) //
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)), //
+                new Triangle(new Point(-150, -150, -115), new Point(-70, 70, -140), new Point(75, 75, -150)) //
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)), //
+                new Sphere(new Point(60, 50, -50), 30d).setEmission(new Color(BLUE)) //
+                        .setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(30).setKt(0.6)));
 
-		scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(0, 0, -1)) //
-				.setKl(4E-5).setKq(2E-7));
+        scene.lights.add(new SpotLight(new Color(700, 400, 400), new Point(60, 50, 0), new Vector(0, 0, -1)) //
+                .setKl(4E-5).setKq(2E-7));
 
-		ImageWriter imageWriter = new ImageWriter("refractionShadow", 600, 600);
-		camera.setImageWriter(imageWriter) //
-				.setRayTracer(new RayTracerBasic(scene)) //
-				.renderImage() //
-				.writeToImage();
-	}
+        ImageWriter imageWriter = new ImageWriter("refractionShadow", 600, 600);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scene)) //
+                .renderImage() //
+                .writeToImage();
+    }
+
+    @Test
+    public void testVariousObjects() {
+        Camera camera = new Camera(new Point(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, 1, 0)) //
+                .setVPSize(200, 200).setVPDistance(1000);
+        scene = new Scene("Test scene");
+
+        scene.setAmbientLight(new AmbientLight(new Color(BLUE).reduce(2), new Double3(0.15)));
+
+        scene.geometries.add(
+                new Triangle(new Point(0, 0, 75), new Point(-150, 150, 135), new Point(150, 150, 135)) //
+                        .setMaterial(new Material().setKd(0.5).setKs(0.5).setKr(0.6).setShininess(60)),
+                new Sphere(new Point(30, 30, -50), 9).setEmission(new Color(RED).reduce(2))
+                .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(60)));//
+
+        scene.addLight(new DirectionalLight(new Color(WHITE).reduce(2), new Vector(0, 0, 1)));
+        scene.addLight(
+                new SpotLight(new Color(YELLOW).reduce(2), new Point(-100, 100, -100), new Vector(0, 0, -1)));
+
+        ImageWriter imageWriter = new ImageWriter("VariousObjects", 600, 600);
+        camera.setImageWriter(imageWriter) //
+                .setRayTracer(new RayTracerBasic(scene)) //
+                .renderImage() //
+                .writeToImage();
+
+    }
 }
+
